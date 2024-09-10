26359b7f78421bae4f67a308e5f6c1f04fc5beb4
JoaoPaulo-creator <joaosarto@gmail.com>
2024-09-10 00:01:19 -0300

    refactor: calling compile file in main.c

--- main.c
+++ main.c
@@ -1,6 +1,19 @@
 #include <stdio.h>
+#include "compiler.h"
 
 int main() {
+	int res = compileFile("./test.c", "./test", 0);
+
+	if (res == COMPILER_FILE_COMPILED_OK) {
+		printf("Compilou certinho caraio\n");
+	} else if (res == COMPILER_FAILED_WITH_ERRORS) {
+		printf("Deu ruim na compilação\n");
+	} else {
+		printf("Sei lá o que ta rolando, tio\n");
+	}
+
+		printf("Compilou certinho caraio\n");
+
 	printf("Hello world\n");
 	return 2;
 }
